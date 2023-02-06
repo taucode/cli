41b3bd9155cb9139f45550ab4e55de5cf4514a91
Andrey Kovalenko <ak@taucode.com>
2023-02-06 15:39:37 +0200

    Winter resurrection - 2023.

--- pre-deploy.cmd
+++ pre-deploy.cmd
@@ -1,9 +1,9 @@
 dotnet restore
 
-dotnet build --configuration Debug
-dotnet build --configuration Release
+dotnet build TauCode.Cli.sln -c Debug
+dotnet build TauCode.Cli.sln -c Release
 
-dotnet test -c Debug .\test\TauCode.Cli.Tests\TauCode.Cli.Tests.csproj
-dotnet test -c Release .\test\TauCode.Cli.Tests\TauCode.Cli.Tests.csproj
+dotnet test TauCode.Cli.sln -c Debug
+dotnet test TauCode.Cli.sln -c Release
 
 nuget pack nuget\TauCode.Cli.nuspec
